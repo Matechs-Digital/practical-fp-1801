596897a17f96e92d1c3f4c7478cbf3af8865e6cf
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-18 17:17:44 +0000

    add tracing example

---
+++ src/10-tracing/index.ts
@@ -0,0 +1,27 @@
+import * as T from "@effect-ts/core/Effect";
+import * as R from "@effect-ts/node/Runtime";
+import { pipe } from "@effect-ts/core/Function";
+
+pipe(
+  T.succeed(1),
+  T.chain((n) => {
+    return T.succeed(n + 1);
+  }),
+  T.chain((n) => {
+    return T.succeed(n + 1);
+  }),
+  T.chain((n) => {
+    return T.succeed(n + 1);
+  }),
+  T.tap((n) => {
+    return T.fail(`(${n})`);
+  }),
+  T.catchAll(function handle(n) {
+    return T.succeed(n);
+  }),
+  T.chain((n) => {
+    return T.fail(`error: ${n}`);
+  }),
+  T.chain(() => T.succeed(0)),
+  R.runMain
+);
